1f259409a9a3591c511c3488408797008921f115
VICTOR MITINGI <mitingi.victor@gmail.com>
2023-04-21 09:54:24 +0300

    complete lab

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-function Comment(props) {
-  return <div className="comment">{props.commentText}</div>;
-}
+function Comment(props) 
+  return <div>{props.commentText}</div>;
 
 export default Comment;
